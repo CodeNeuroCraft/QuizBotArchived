8558ee1f768710c0f56decbff2887400fd99c6b4
WESHOK <gdvarta@gmail.com>
2024-05-10 10:12:53 +0300

    now aiogram lib is used

--- main.py
+++ main.py
@@ -1,35 +1,56 @@
-import telebot
+import asyncio
+import logging
+import sys
+from os import getenv
 
-from telebot import types
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
 
-# Замените 'YOUR_BOT_TOKEN' на токен вашего бота
-bot = telebot.TeleBot('7031635922:AAEwZ1BM3S9A2j4Ja8Z3C2t8K6ED3lh4654')
+# Bot token can be obtained via https://t.me/BotFather
+TOKEN = getenv("7031635922:AAEwZ1BM3S9A2j4Ja8Z3C2t8K6ED3lh4654")
 
-# Обработчик команды '/start', который запускает меню
-@bot.message_handler(commands=['start'])
-def menu(message):
-    # Создание клавиатуры
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-    # Добавление кнопок
-    markup.row('/start', '/help')
-    markup.row('/settings', '/profile')
-    # Отправка сообщения с клавиатурой
-    bot.send_message(message.chat.id, "Выберите команду из меню:", reply_markup=markup)
+# All handlers should be attached to the Router (or Dispatcher)
+dp = Dispatcher()
 
-# Обработчик команды '/help'
-@bot.message_handler(commands=['help'])
-def help_command(message):
-    bot.send_message(message.chat.id, "Это раздел помощи. Здесь вы можете получить информацию о боте.")
 
-# Обработчик команды '/settings'
-@bot.message_handler(commands=['settings'])
-def settings(message):
-    bot.send_message(message.chat.id, "Это настройки вашего профиля.")
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receives messages with `/start` command
+    """
+    # Most event objects have aliases for API methods that can be called in events' context
+    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
+    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
+    # method automatically or call API method directly via
+    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
+    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
 
-# Обработчик команды '/profile'
-@bot.message_handler(commands=['profile'])
-def profile(message):
-    bot.send_message(message.chat.id, "Это ваш профиль.")
 
-# Запуск бота
-bot.polling()
+@dp.message()
+async def echo_handler(message: Message) -> None:
+    """
+    Handler will forward receive a message back to the sender
+
+    By default, message handler will handle all message types (like a text, photo, sticker etc.)
+    """
+    try:
+        # Send a copy of the received message
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        # But not all the types is supported to be copied so need to handle it
+        await message.answer("Nice try!")
+
+
+async def main() -> None:
+    # Initialize Bot instance with default bot properties which will be passed to all API calls
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    # And the run events dispatching
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
